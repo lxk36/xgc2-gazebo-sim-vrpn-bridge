cefa01ede3a00604ac99aa233ef2d56e08113472
Lxk_buaa <867768510@qq.com>
2026-07-10 23:09:51 +0800

    fix: align packaged dependency versions

--- .xgc2/scripts/check_package_compliance.sh
+++ .xgc2/scripts/check_package_compliance.sh
@@ -39,7 +39,7 @@ grep -q "id: xgc2-gazebo-sim-vrpn-bridge" .xgc2/product.yml
 grep -Eq '^version: [0-9]+\.[0-9]+\.[0-9]+-[0-9]+$' .xgc2/product.yml
 grep -q "ros-noetic-xgc2-gazebo-sim-vrpn-bridge" .xgc2/product.yml
 grep -q "PACKAGE=\"ros-noetic-xgc2-gazebo-sim-vrpn-bridge\"" .xgc2/scripts/package_debs.sh
-grep -q "libxgc2-math-dev (>= 0.5.1-1)" .xgc2/scripts/package_debs.sh
+grep -q "libxgc2-math-dev (>= 0.5.6-5~focal)" .xgc2/scripts/package_debs.sh
 grep -q "gazebo_sim_vrpn_bridge" package.xml
 grep -q "gazebo_vrpn_server_node" CMakeLists.txt
 grep -q "vrpn_server.launch" .xgc2/scripts/check_installed_packages.sh
